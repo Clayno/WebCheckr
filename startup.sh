654467025003a864646eedb298dd8a801f336f74
Clayno <15702611+Clayno@users.noreply.github.com>
2019-11-18 09:58:52 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,8 +5,8 @@ docker network create webcheckr
 docker build -t webcheckr .
 docker create --net=webcheckr --name cvesearch_docker ttimasdf/cve-search:withdb
 
-echo "Creating file in /usr/local/sbin/webcheckr (need sudo)"
-sudo tee -a /usr/local/sbin/webcheckr<<EOF
+echo "Creating file in /usr/local/bin/webcheckr (need sudo)"
+sudo tee -a /usr/local/bin/webcheckr<<EOF
 #!/bin/bash
 
 docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v ${PWD}:/webcheckr --user $(id -u):$(id -g) --group-add $(stat -c '%g' /var/run/docker.sock) --net webcheckr webcheckr $@
