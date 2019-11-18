0cde84ddb30cfaa548a384e281cabbf391b7a92f
Clayno <15702611+Clayno@users.noreply.github.com>
2019-11-18 10:03:01 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -9,7 +9,7 @@ echo "Creating file in /usr/local/bin/webcheckr (need sudo)"
 sudo tee -a /usr/local/bin/webcheckr<<EOF
 #!/bin/bash
 
-docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v ${PWD}:/webcheckr --user $(id -u):$(id -g) --group-add $(stat -c '%g' /var/run/docker.sock) --net webcheckr webcheckr $@
+docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v \${PWD}:/webcheckr --user \$(id -u):\$(id -g) --group-add \$(stat -c '%g' /var/run/docker.sock) --net webcheckr webcheckr $@
 
 EOF
 sudo chmod u+x /usr/local/bin/webcheckr
